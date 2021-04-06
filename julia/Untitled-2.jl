172c71ca5cf27041a423bfa81699198448fbde4f
MarinaPaley <marina_paley@mail.ru>
2021-04-06 21:39:12 +0300

    Внесены исправления по замечаниям к PR

--- julia/Untitled-2.jl
+++
@@ -1,7 +0,0 @@
-println("hello world")
-x = -2.9;
-y = 15.5;
-z = 1.5;
-a = sqrt(x * x + y) - y * y * sin.(( x + z ) / x)^3;
-b=cos(x^3)^2 - x / sqrt(z * z + y * y);
-println("x =", x, " y = ", y, " z ", z, "a = ", a, " b = ",b);
