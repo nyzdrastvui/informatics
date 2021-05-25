42d856dddce85f9514355b35784ba88011e1eeef
nyzdrastvui <mister.disanasan2013@yandex.ru>
2021-05-25 22:55:57 +0300

    решение задачи 2-1, 5 вариант

--- julia 5 вариант/x = -2.9;.jl
+++ julia 5 вариант/x = -2.9;.jl
@@ -1,13 +1,47 @@
-println("Задание 1-2");
+include("input_int.jl");
+include("input_double.jl");
+include("is_correct.jl");
+include("get_rectangle_area.jl");
+include("get_triangle_area.jl");
+include("is_valid_rectangle.jl");
+include("is_valid_triangle.jl");
 
-x = -2.9;
-y = 15.5;
-z = 1.5;
+println("Task 2-1");
 
-println("Константы:");
-println("\tx = $x;\n\ty = $y;\n\tz = $z;\n");
+# Введем перечислимый тип данных
+@enum Shape rectangle = 1 triangle = 2
+# Ввод пользоавателем номера фигуры
+user_input = input_int("Введите число, для выбора фигуры: " *
+                        "$(Int(rectangle::Shape)) - прямоугольник, " *
+                        "$(Int(triangle::Shape)) - треугольник");
+# Проверка и расчет
+try
+    local figure = Shape(user_input);
+    # расчет площади прямоугольника
+    if figure == rectangle::Shape
+        local width = input_double("Введите ширину прямоугольника ");
+        local length = input_double("Введите длину прямоугольника ");
 
-a = sqrt(x * x + y) - y * y * sin((x + z) / x)^3;
-b = cos(x^3)^2 - x / sqrt(z * z + y * y);
+        if is_valid_rectangle(width, length)
+            local rectangle_area = get_rectangle_area(width, length);
+            println("Площадь прямоугольника $rectangle_area");
+        else
+            println("Ошибка ввода данных для прямоугольника");
+    end;
+    # расчет площади треугольника
+    else
+        local a = input_double("Введите сторону треугольника");
+        local b = input_double("Введите сторону треугольника");
+        local c = input_double("Введите сторону треугольника");
 
-println("Результаты:" "a = ", a, "b = ", b); 
+        if is_valid_triangle(a, b, c)
+            local triangle_area = get_triangle_area(a, b, c);
+            println("Площадь треугольника $triangle_area");
+        else
+            println("Ошибка ввода данных для треугольника");
+        end;
+    end;
+        # обработка ошибки ввода
+catch
+    println("Ошибка ввода выбора фигуры");
+end;
